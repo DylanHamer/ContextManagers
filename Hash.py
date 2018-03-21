5168c81e7f0bf3f349a1eda04824eef5e252938d
Dylan Hamer <dylanhamer13@gmail.com>
2018-03-21 12:58:50 +0000

    Create Hash.py

---
+++ Hash.py
@@ -0,0 +1,18 @@
+"""
+Hash
+Use a context manager to create a hashed string
+by Dylan Hamer
+"""
+
+from contextlib import contextmanager
+from hashlib import sha256
+
+@contextmanager
+def hashString(text):
+    encodedString = text.encode()
+    hashObject = sha256(encodedString)
+    yield hashObject
+
+with hashString("Hello, World!") as hashObject:
+    print(hashObject.hex_digest())
+    
