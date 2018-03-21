7f94239543fb266148f698ff5daf04f3c8b81d9d
Dylan Hamer <dylanhamer13@gmail.com>
2018-03-21 13:25:01 +0000

    Update Hash.py

--- Hash.py
+++ Hash.py
@@ -6,13 +6,26 @@ by Dylan Hamer
 
 from contextlib import contextmanager
 from hashlib import sha256
+import types
 
+class hash(object):
+    def __init__(self, hashObject):
+        self.hashObject = hashObject
+        
+    def hexdigest(self):
+        """Wrapper for original hash object's hexdigest function"""
+        return self.hashObject.hexdigest()
+        
+    def compare(self, newHash):
+        """Compare two hash hexdigests"""
+        comparison = newHash is hashObject
+        return comparison
+        
 @contextmanager
 def hashString(text):
     encodedString = text.encode()
     hashObject = sha256(encodedString)
-    yield hashObject
-
-with hashString("Hello, World!") as hashObject:
-    print(hashObject.hexdigest())
+    yield hash(hashObject)
     
+with hashString("Hello, World!") as hashObject:
+    print hashObject.hexdigest()
